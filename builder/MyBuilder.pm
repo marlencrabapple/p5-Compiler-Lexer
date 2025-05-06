3c54cc5b5a505819e5e54b82cc95d17673159f81
Ian P Bradley <ian.bradley@studiocrabapple.com>
2025-05-05 21:22:17 -0700

    Add  `-Wno-register` to builder::MyBuilder fix buildilng with ISO C++17

--- builder/MyBuilder.pm
+++ builder/MyBuilder.pm
@@ -1,15 +1,24 @@
-package builder::MyBuilder;
-use strict;
-use warnings FATAL => 'all';
-use 5.008005;
-use base 'Module::Build::XSUtil';
-use constant DEBUG => 0;
+use Object::Pad ':experimental(:all)';
 
-sub new {
-    my ( $class, %args ) = @_;
-    my @ignore_warnings_options = map { "-Wno-$_" } qw(missing-field-initializers);
-    my $self = $class->SUPER::new(
-        %args,
+package builder::MyBuilder;
+class builder::MyBuilder :isa(Module::Build::XSUtil);
+
+use utf8;
+use v5.40;
+
+use base 'Module::Build::XSUtil';
+
+use Const::Fast;
+const our $DEBUG => 1;
+
+method $on_new : common ($params) {
+  my @ignore_warnings_options
+      = map { "-Wno-$_" } qw(missing-field-initializers
+                             register
+                             misleading-indentdation);
+
+  my $self = $class->new(
+        %$params,
         generate_ppport_h    => 'include/ppport.h',
         needs_compiler_cpp   => 1,
         c_source => [qw/src/],
@@ -18,11 +27,18 @@ sub new {
         extra_compiler_flags => ['-Iinclude', @ignore_warnings_options, '-g3'],
         add_to_cleanup => [
             'lib/Compiler/Lexer/*.o', 'lib/Compiler/Lexer/*.c',
-            'lib/Compiler/Lexer/*.xs',
-        ],
-    );
-    $self->{config}->set('optimize' => '-O0') if (DEBUG);
-    return $self;
-}
+            'lib/Compiler/Lexer/*.xs'
+        ]
+  );
 
-1;
+  $self->{config}->set('optimize' => '-O0') if $DEBUG;
+
+  $self 
+};
+
+
+ADJUSTPARAMS ($params) {
+  __CLASS__->$on_new($params)
+};
+
+
